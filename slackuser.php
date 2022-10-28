c1aea6eb4950fb6214f30c5a876332c2206a7ac3
oyinkansola <yahayaoyinkansola@gmail.com>
2022-10-28 17:01:42 +0100

    initial commit

---
+++ slackuser.php
@@ -0,0 +1,7 @@
+<?php
+    echo json_encode([
+        "slackUsername" => "Kansoldev",
+        "backend" => true,
+        "age" => 22,
+        "bio" => "I am a back-end developer building products that scale"
+    ]);
